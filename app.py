cd00f8692a3d480ea8b7ce4fea2c069c1bac9615
Your Name <you@example.com>
2023-04-07 20:04:23 +0530

    bug resolve

--- app.py
+++ app.py
@@ -7,28 +7,28 @@ import streamlit as st
 #from io import StringIO
 
 st.title("Potato Disease Prediction Application🥔")
-st.subheader("Developed by Group Q8🪄")
+st.caption("Developed by Group Q8🪄")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     with st.columns(3)[1]:
-        st.image(uploaded_file,width=300, caption='Enter any caption here')
+        st.image(uploaded_file,width=200,)
 
 
-test_image = tf.keras.utils.load_img(uploaded_file,target_size=(256,256))
+    test_image = tf.keras.utils.load_img(uploaded_file,target_size=(256,256))
 
 
-st.subheader(":red[Prediction:] ")
-model=load_model('potatoes.h5')
+    st.subheader(":red[Prediction:] ")
+    model=load_model('potatoes.h5')
 
-class_names=['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
-classes=['Early Blight Disease', 'Late Blight Disease', 'healthy leaf']
+    class_names=['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+    classes=['Early Blight Disease', 'Late Blight Disease', 'healthy leaf']
 
-test_image = tf.keras.utils.img_to_array(test_image)
-test_image = np.expand_dims(test_image,axis=0)
+    test_image = tf.keras.utils.img_to_array(test_image)
+    test_image = np.expand_dims(test_image,axis=0)
 
-# Result array
-result = model.predict(test_image)
-confidence=round(100 * (np.max(result)), 2)
-prediction=(classes[np.argmax(result)])
-st.subheader(prediction)
-"_Confidence_= ",confidence,'%'
+    # Result array
+    result = model.predict(test_image)
+    confidence=round(100 * (np.max(result)), 2)
+    prediction=(classes[np.argmax(result)])
+    st.subheader(prediction)
+    "_Confidence_= ",confidence,'%'
